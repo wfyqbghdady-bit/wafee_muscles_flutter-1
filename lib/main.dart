1fb4fe267b7a232644187848fa06f7cba02968ce
wfyqbghdady-bit <wfyqbghdady@gmail.com>
2026-02-12 00:41:17 +0200

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/workout_provider.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => WorkoutProvider(),
+      child: MaterialApp(
+        title: 'Wafee Muscles',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+      home: HomeScreen(), 
+      ),
+    );
+  }
+}
